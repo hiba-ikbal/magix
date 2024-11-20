5b2a880a154b58ca0a48935fc9dc0d4dc943ac26
hiba <cherkaouiikbalhiba@gmail.com>
2024-11-20 17:19:58 -0500

    cards in each section good size maybe bigger? should do js after

--- magix-rendu/game.php
+++ magix-rendu/game.php
@@ -29,8 +29,56 @@ require_once("partials/header.php");
 ?>
 <!-- statut du jeu pour dire si c'est ton tour ou celui de l'ennemi avec btn retour au lobby -->
 
+<!-- details de l'ennemi  -->
+<section id="enemy-section">
+    <div id="enemy-cards-container"></div>
+    <div id="enemy-details">
+        <div class="enemy-profile-container">
+            <!-- <img id="enemy-avatar" alt="Enemy photo"/> -->
+            <div class="enemy-user"></div>
+        </div>
+        <div class="enemy-hp-mp-container">
+            <div class="enemy-hp"></div>
+            <div class="enemy-mp"></div>
+        </div>
+    </div>
+    <div class="enemy-remaining-cards-container">
+        <img src="assets/img1.jpg" alt="Card Remaining" />
+        <div id="enemy-remaining-cards-qty"></div>
+    </div>
+</section>
+  <!-- details du player -->
+  <section id="player-section">
+    <div id="player-details">
+        <div class="player-remaining-cards-container">
+            <img src="assets/img1.jpg" alt="Player remaining cards">
+            <div class="player-remaining-cards"></div>
+        </div>
+        <div class="player-hp-mp-container">
+            <div class="player-hp"></div>
+            <div class="player-mp"></div>
+        </div>
+    </div>
+    <div id="player-cards-container"></div>
+    <div class="player-actions-container">
+        <button id="hero-power">Hero Power</button>
+        <h3 id="remaining-turn-time">Remaining turn time</h3>
+        <h3 id="your-turn"></h3>
+        <button id="end-turn">End Turn</button>
+    </div>
+</section>
+<template id="cards-template">
+    <img class="card-template-photo"/>
+    <h2></h2>
+    <div id="cards-template-stats-container">
+        <div class='attack'></div>
+        <div class='hp'></div>
+        <div class='cost'></div>
+    </div>
+    <div class='mechanics'></div>
+</template>
 
-  
+
 
     <?php
 	require_once("partials/footer.php");
